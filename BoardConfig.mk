e376bab4aaaa2f4a2be50f2b57e97f355baed0d2
Wolfgang Hobmaier <0woh0.android@gmail.com>
2013-01-22 15:39:47 +0100

    Initial commit
    Based on Evervolv/device/htc/bravo & Evervolv/device/htc/qsd8k-common

---
+++ BoardConfig.mk
@@ -0,0 +1,156 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/bravo/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := bravo
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 msmsdcc_sdioirq=1 wire.search_count=5
+BOARD_KERNEL_BASE := 0x20000000
+BOARD_KERNEL_NEW_PPPOX := true
+
+TARGET_KERNEL_CONFIG    := evervolv_bravo_defconfig
+TARGET_PREBUILT_KERNEL  := device/htc/bravo/prebuilt/kernel
+
+# to enable the GPS HAL
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := bravo
+
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
+
+# Pull all dictionaries
+TARGET_USE_KEYBOARD := international
+
+# Use legacy touchscreen
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# Hacks
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+BOARD_USE_LEGACY_TRACKPAD := true
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 000a0000 00020000 "misc"
+# mtd1: 00500000 00020000 "recovery"
+# mtd2: 00280000 00020000 "boot"
+# mtd3: 0fa00000 00020000 "system"
+# mtd4: 02800000 00020000 "cache"
+# mtd5: 093a0000 00020000 "userdata"
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00280000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE :=  262144000 # 0x09100000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 154140672 # 0x093a0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_NO_BOOTLOADER := true
+
+# QSD8250
+TARGET_BOARD_PLATFORM := qsd8k
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Arm (v7a) w/ neon
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Scorpion
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := device/htc/bravo/include
+
+# Wifi
+WIFI_BAND                        := 802_11_ABG
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+BOARD_LEGACY_NL80211_STA_EVENTS := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+
+# Compass/Accelerometer
+BOARD_VENDOR_USE_AKMD := akm8973
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/bravo/bluetooth/vnd_qsd8k.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/bravo/bluetooth/include
+
+# Fm radio
+#BOARD_HAVE_FM_RADIO := true
+#BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+# Qcom
+BOARD_USES_LEGACY_QCOM := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
+
+# Hardware rendering
+BOARD_EGL_CFG := device/htc/bravo/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+# We only have 2 buffers
+TARGET_DISABLE_TRIPLE_BUFFERING := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+TARGET_NO_HW_VSYNC := true
+COMMON_GLOBAL_CFLAGS += -DTARGET_8x50
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+# Webkit
+TARGET_FORCE_CPU_UPLOAD := true
+ENABLE_WEBGL := true
+
+# Legacy ril
+COMMON_GLOBAL_CFLAGS += -DRIL_VERSION_2_SUPPORT
+
+# Camcorder
+BOARD_USE_OLD_AVC_ENCODER := true
+BOARD_NO_BFRAMES := true
+
+# Kernel directory
+TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
+BUILD_KERNEL := true
+
+#BOARD_USES_LEGACY_CAMERA := true
+
+# Override kernel toolchain. (4.6 is too unstable)
+KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.4.3/bin/arm-eabi-
+
+# Use old kernel toolchain.  GCC 4.6 may be the root cause of our modem
+# interface dying all the time.
+# TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
---
+++ bravo.mk
@@ -0,0 +1,227 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM bravo,
+# not specialized for any geography.
+#
+
+# First, the most specific values, i.e. the aspects that are specific to GSM
+
+DEVICE_PACKAGE_OVERLAYS := device/htc/bravo/overlay
+
+# Propreties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    rild.libpath=/system/lib/libhtc_ril.so \
+    ro.ril.ecc.HTC-ELL=92,93,94 \
+    ro.ril.ecc.HTC-WWE=999 \
+    ro.ril.enable.a52.HTC-ITA=1 \
+    ro.ril.enable.a53.HTC-ITA=1 \
+    ro.ril.enable.a52=0 \
+    ro.ril.enable.a53=1 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.hsupa.category=5 \
+    ro.ril.hsxpa=2 \
+    ro.ril.def.agps.mode=2 \
+    ro.ril.disable.power.collapse=0 \
+    windowsmgr.max_events_per_sec=120 \
+    mobiledata.interfaces=rmnet0,rmnet1,rmnet2 \
+    ro.media.dec.jpeg.memcap=20000000 \
+    ro.opengles.version=131072 \
+    ro.telephony.default_network=3 \
+    ro.ril.enable.prl.recognition=1 \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
+    ro.ril.emc.mode=2 \
+    ro.telephony.ril.v3=signalstrengthgsm,singlepdp,apptypesim \
+    ro.vold.umsdirtyratio=20
+
+# Set usb type
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.sys.usb.config=mass_storage \
+    persist.service.adb.enable=1
+
+#
+# Packages required for bravo
+#
+
+# Sensors
+PRODUCT_PACKAGES += \
+    gps.bravo \
+    lights.bravo \
+    sensors.bravo
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/init.bravo.rc:root/init.bravo.rc \
+    device/htc/bravo/init.bravo.usb.rc:root/init.bravo.usb.rc \
+    device/htc/bravo/ueventd.bravo.rc:root/ueventd.bravo.rc
+
+# Prebuilt files/configs
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/bravo-keypad.kl:system/usr/keylayout/bravo-keypad.kl \
+    device/htc/bravo/bravo-keypad.kcm:system/usr/keychars/bravo-keypad.kcm \
+    device/htc/bravo/curcial-oj.idc:system/usr/idc/curcial-oj.idc \
+    device/htc/bravo/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/bravo/h2w_headset.kcm:system/usr/keychars/h2w_headset.kcm \
+    device/htc/bravo/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/bravo/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
+    device/htc/bravo/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
+    device/htc/bravo/vold.fstab:system/etc/vold.fstab \
+    device/htc/bravo/sysctl.conf:system/etc/sysctl.conf
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/media_profiles.xml:system/etc/media_profiles.xml
+
+# Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/bravo/bravo-vendor.mk)
+
+# English locale
+PRODUCT_LOCALES := en
+
+# High Density art
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Configs
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/media_codecs.xml:system/etc/media_codecs.xml \
+    device/htc/bravo/audio_policy.conf:system/etc/audio_policy.conf
+
+# Misc
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/init.power.rc:root/init.power.rc
+
+#
+# Required Packages
+#
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.usb.default \
+    audio.a2dp.default \
+    audio.primary.qsd8k \
+    audio_policy.qsd8k
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.qsd8k
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.qsd8k \
+    gralloc.qsd8k \
+    hwcomposer.qsd8k
+
+# Omx
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libstagefrighthw
+#    libOmxVidEnc \
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# Misc
+PRODUCT_PACKAGES += \
+    power.qsd8k \
+    com.android.future.usb.accessory
+
+#
+# Hardware Rendering Properties
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.composition.type=mdp \
+    debug.gr.numframebuffers=2
+
+#
+# Dalvik Properties
+#
+
+# dexop-flags: "v=" n|r|a, "o=" n|v|a|f, "m=y" register map
+# v=verify o=optimize: n=none r=remote a=all f=full v=verified
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.checkjni=false
+
+# Default heap settings for 512mb device
+include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+#
+# Camera (video recording)
+#
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.camcorder.disablemeta=1 \
+    rw.media.record.hasb=0
+
+#
+# Wifi
+#
+
+# Firmware
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=45
+
+#
+# Qcom
+#
+
+# Init post-boot script
+PRODUCT_COPY_FILES += \
+    device/htc/bravo/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
+
+#
+# Permissions
+#
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distict.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Proprietary blobs
+$(call inherit-product-if-exists, vendor/htc/qsd8k-common/qsd8k-vendor.mk)
